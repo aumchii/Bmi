aba9da0201e5e1f500b2e3262af915fe3e9e3b3d
aumchii <60050148@kmitl.ac.th>
2018-09-11 13:27:06 +0700

    Bmi

---
+++ Bmi.java
@@ -0,0 +1,17 @@
+import java.util.*; 
+class Bmi 
+{
+	public static void main(String[] args) 
+	{   
+		Scanner sc=new Scanner (System.in);
+
+		System.out.print("weight:  "); 
+			double w=sc.nextInt(); 
+		System.out.print("height:  "); 
+		    double h=sc.nextInt(); 
+        
+		//System.out.printf("Your BMI :    "+w/(h*h));
+		System.out.printf("%s%5.2f","Your BMI :   ",w/(h*h);
+		
+	}
+}
